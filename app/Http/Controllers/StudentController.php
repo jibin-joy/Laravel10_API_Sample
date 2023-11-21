3256862ca15e134541ea50fab4adf815a4a8667f
Jibin-Sample <jibinkjoy3101@gmail.com>
2023-11-21 21:47:53 +0530

    Laravel10-Sample-Api-Project

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function index(){
+        $student = Student::all();
+        $data = [
+            'status' => 200,
+            'student' =>$student
+        ];
+        return response()->json($data,200);
+    }
+}
